4bde48fd8cce48b1e6b894a3f0df2a21403ac0e3
Luke Pillow <lpillow@gmail.com>
2010-02-23 22:55:07 -0600

    Changing to use Facebox

--- content/javascripts/rubymidwest.js
+++ content/javascripts/rubymidwest.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-  $(".submit-proposal").colorbox({width:"80%", height:"80%", iframe:true});
+  $(".submit-proposal").click(function(e) {
+    e.preventDefault();
+    $.facebox($('<iframe src="http://spreadsheets.google.com/embeddedform?formkey=dG1ZTUYyVW5sRFN3U0FFQy1ZaFQwMGc6MA" width="570" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'));
+  });
 });
 
 
