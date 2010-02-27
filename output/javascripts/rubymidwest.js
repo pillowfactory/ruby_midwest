4e7c779fc8721d74ebdcca6d780478d3f93d859a
Luke Pillow <lpillow@gmail.com>
2010-02-27 13:27:24 -0600

    Added the ability to link to /#call-for-speakers that will automatically popup the speaker proposal form.

--- output/javascripts/rubymidwest.js
+++ output/javascripts/rubymidwest.js
@@ -1,8 +1,13 @@
 $(document).ready(function() {
+  var iframe = '<iframe src="http://spreadsheets.google.com/embeddedform?formkey=dG1ZTUYyVW5sRFN3U0FFQy1ZaFQwMGc6MA" width="570" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'
+
   $(".submit-proposal").click(function(e) {
     e.preventDefault();
-    $.facebox($('<iframe src="http://spreadsheets.google.com/embeddedform?formkey=dG1ZTUYyVW5sRFN3U0FFQy1ZaFQwMGc6MA" width="570" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'));
+    $.facebox($(iframe));
   });
+
+  if(window.location.hash.indexOf('call-for-speakers') != -1)
+      $.facebox($(iframe));
 });
 
 
